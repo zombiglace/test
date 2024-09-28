24169e4e16ca1634c1c8addd181a15ce5e44383c
Zombiglace Gaming <zombiglacegaming@gmail.com>
2024-09-28 19:48:45 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+  console.log("New client connected");
+
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+    console.log("message:" + msg);
+  });
+});
+
+server.listen(3000, () => {
+  console.log("Server is running on port 3000...");
+});
