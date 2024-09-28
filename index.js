2b2f9d4a2a69de40565f39e0b31ec31e96519b6f
zombiglace <101476751+zombiglace@users.noreply.github.com>
2024-09-28 21:45:22 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const server = require("http").createServer(app);
 const io = require("socket.io")(server);
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "./src/index.html");
+  res.sendFile(__dirname + "/index.html");
 });
 
 io.on("connection", (socket) => {
