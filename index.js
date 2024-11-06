bb2047feb7e0424c0ac81eb46b7958e22c22a48e
zombiglace <101476751+zombiglace@users.noreply.github.com>
2024-11-06 14:27:29 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const server = require("http").createServer(app);
 const io = require("socket.io")(server);
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/chat.html");
+  res.sendFile(__dirname + "/index.html");
 });
 
 io.on("connection", (socket) => {
